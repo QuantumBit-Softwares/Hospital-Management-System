2dcf4aaea66d33c92b8b35f68b39576a94652d4a
asher-lab <asher01000001@gmail.com>
2021-06-24 20:50:10 +0800

    clean system without binary search

--- myhmsdb.sql
+++ myhmsdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 16, 2020 at 02:34 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2021 at 02:48 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -116,16 +115,15 @@ INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 
 CREATE TABLE `doctb` (
   `username` varchar(50) NOT NULL,
-   `gender` varchar(10) NOT NULL,
+  `gender` varchar(10) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `spec` varchar(50) NOT NULL,
-  `docFees` BIGINT(10) NOT NULL,
-  `lat` float(10) NOT NULL,
-  `lng` float(10) NOT NULL,
-  `docContact` BIGINT(20) NOT NULL,
+  `docFees` bigint(10) NOT NULL,
+  `lat` float NOT NULL,
+  `lng` float NOT NULL,
+  `docContact` bigint(20) NOT NULL,
   `docAddress` varchar(100) NOT NULL
- 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -133,15 +131,14 @@ CREATE TABLE `doctb` (
 --
 
 INSERT INTO `doctb` (`username`, `gender`, `password`, `email`, `spec`, `docFees`, `lat`, `lng`, `docContact`, `docAddress`) VALUES
-('ashok', 'Male', 'ashok123', 'ashok@gmail.com', 'General', 500, 14.5995, 120.9842, 09123456789, 'Manila' ),
-('arun', 'Male','arun123', 'arun@gmail.com', 'Cardiologist', 600,  14.5995, 120.9842, 08123456789, 'Tondo'),
-('Dinesh', 'Male','dinesh123', 'dinesh@gmail.com', 'General', 700,  14.5995, 120.9842, 02882223223, 'Zambales'),
-('Ganesh', 'Male','ganesh123', 'ganesh@gmail.com', 'Pediatrician', 550,  14.5995, 120.9842, 07123456789, 'Clark'),
-('Kumar', 'Male','kumar123', 'kumar@gmail.com', 'Pediatrician', 800, 14.5995, 120.9842, 19123456789, 'Pampanga'),
-('Amit', 'Female','amit123', 'amit@gmail.com', 'Cardiologist', 1000,  14.5995, 120.9842, 39123456789, 'Clark City'),
-('Abbis','Female', 'abbis123', 'abbis@gmail.com', 'Neurologist', 1500, 14.5995, 120.9842, 19123456789, 'Clark Avenue'),
-('Tiwary', 'Female','tiwary123', 'tiwary@gmail.com', 'Pediatrician', 450,  14.5995, 120.9842, 33123456789, 'Clark New York');
-
+('ashok', 'Male', 'ashok123', 'ashok@gmail.com', 'General', 500, 15.5645, 120.423, 9123456789, 'Manila'),
+('arun', 'Male', 'arun123', 'arun@gmail.com', 'Cardiologist', 600, 13.9833, 120.857, 8123456789, 'Tondo'),
+('Dinesh', 'Male', 'dinesh123', 'dinesh@gmail.com', 'General', 700, 14.6012, 121.011, 2882223223, 'Zambales'),
+('Ganesh', 'Male', 'ganesh123', 'ganesh@gmail.com', 'Pediatrician', 550, 14.3601, 120.961, 7123456789, 'Clark'),
+('Kumar', 'Male', 'kumar123', 'kumar@gmail.com', 'Pediatrician', 800, 14.9323, 120.107, 19123456789, 'Pampanga'),
+('Amit', 'Female', 'amit123', 'amit@gmail.com', 'Cardiologist', 1000, 14.0516, 121.514, 39123456789, 'Clark City'),
+('Abbis', 'Female', 'abbis123', 'abbis@gmail.com', 'Neurologist', 1500, 14.6249, 120.541, 19123456789, 'Clark Avenue'),
+('Tiwary', 'Female', 'tiwary123', 'tiwary@gmail.com', 'Pediatrician', 450, 13.135, 123.668, 33123456789, 'Clark New York');
 
 -- --------------------------------------------------------
 
@@ -150,7 +147,7 @@ INSERT INTO `doctb` (`username`, `gender`, `password`, `email`, `spec`, `docFees
 --
 
 CREATE TABLE `patreg` (
-  `pid` int(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `pid` int(11) NOT NULL,
   `fname` varchar(20) NOT NULL,
   `lname` varchar(20) NOT NULL,
   `gender` varchar(10) NOT NULL,
@@ -158,8 +155,8 @@ CREATE TABLE `patreg` (
   `contact` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `cpassword` varchar(30) NOT NULL,
-  `lat` float(10) NOT NULL,
-  `lng` float(10) NOT NULL,
+  `lat` float NOT NULL,
+  `lng` float NOT NULL,
   `patAddress` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -167,18 +164,18 @@ CREATE TABLE `patreg` (
 -- Dumping data for table `patreg`
 --
 
-INSERT INTO `patreg` (`pid`, `fname`, `lname`, `gender`, `email`, `contact`, `password`, `cpassword`, `patAddress`) VALUES
-(1, 'Ram', 'Kumar', 'Male', 'ram@gmail.com', '9876543210', 'ram123', 'ram123',  'Germany'),
-(2, 'Alia', 'Bhatt', 'Female', 'alia@gmail.com', '8976897689', 'alia123', 'alia123', 'Germany'),
-(3, 'Shahrukh', 'khan', 'Male', 'shahrukh@gmail.com', '8976898463', 'shahrukh123', 'shahrukh123','Germany'),
-(4, 'Kishan', 'Lal', 'Male', 'kishansmart0@gmail.com', '8838489464', 'kishan123', 'kishan123',  'Germany'),
-(5, 'Gautam', 'Shankararam', 'Male', 'gautam@gmail.com', '9070897653', 'gautam123', 'gautam123',  'Germany'),
-(6, 'Sushant', 'Singh', 'Male', 'sushant@gmail.com', '9059986865', 'sushant123', 'sushant123',  'Germany'),
-(7, 'Nancy', 'Deborah', 'Female', 'nancy@gmail.com', '9128972454', 'nancy123', 'nancy123', 'Germany'),
-(8, 'Kenny', 'Sebastian', 'Male', 'kenny@gmail.com', '9809879868', 'kenny123', 'kenny123',  'Germany'),
-(9, 'William', 'Blake', 'Male', 'william@gmail.com', '8683619153', 'william123', 'william123',  'Germany'),
-(10, 'Peter', 'Norvig', 'Male', 'peter@gmail.com', '9609362815', 'peter123', 'peter123',  'Germany'),
-(11, 'Shraddha', 'Kapoor', 'Female', 'shraddha@gmail.com', '9768946252', 'shraddha123', 'shraddha123',  'Germany');
+INSERT INTO `patreg` (`pid`, `fname`, `lname`, `gender`, `email`, `contact`, `password`, `cpassword`, `lat`, `lng`, `patAddress`) VALUES
+(1, 'Ram', 'Kumar', 'Male', 'ram@gmail.com', '9876543210', 'ram123', 'ram123', 14.0008, 120.857, 'Germany'),
+(2, 'Alia', 'Bhatt', 'Female', 'alia@gmail.com', '8976897689', 'alia123', 'alia123', 14.0886, 121.148, 'Germany'),
+(3, 'Shahrukh', 'khan', 'Male', 'shahrukh@gmail.com', '8976898463', 'shahrukh123', 'shahrukh123', 14.232, 120.769, 'Germany'),
+(4, 'Kishan', 'Lal', 'Male', 'kishansmart0@gmail.com', '8838489464', 'kishan123', 'kishan123', 14.2872, 120.978, 'Germany'),
+(5, 'Gautam', 'Shankararam', 'Male', 'gautam@gmail.com', '9070897653', 'gautam123', 'gautam123', 14.0831, 121.651, 'Germany'),
+(6, 'Sushant', 'Singh', 'Male', 'sushant@gmail.com', '9059986865', 'sushant123', 'sushant123', 14.4668, 120.543, 'Germany'),
+(7, 'Nancy', 'Deborah', 'Female', 'nancy@gmail.com', '9128972454', 'nancy123', 'nancy123', 14.7659, 121.013, 'Germany'),
+(8, 'Kenny', 'Sebastian', 'Male', 'kenny@gmail.com', '9809879868', 'kenny123', 'kenny123', 14.5173, 121.252, 'Germany'),
+(9, 'William', 'Blake', 'Male', 'william@gmail.com', '8683619153', 'william123', 'william123', 14.8513, 120.361, 'Germany'),
+(10, 'Peter', 'Norvig', 'Male', 'peter@gmail.com', '9609362815', 'peter123', 'peter123', 14.0333, 120.889, 'Germany'),
+(11, 'Shraddha', 'Kapoor', 'Female', 'shraddha@gmail.com', '9768946252', 'shraddha123', 'shraddha123', 15.4185, 120.579, 'Germany');
 
 -- --------------------------------------------------------
 
